6f8317a1f17379fa8da70e4eff783e1b990e355d
Jelle Schutter <jelle@schutter.xyz>
2024-07-06 21:20:17 +0200

    feat: add jobs section (#295)
    * feat: add jobs section
    
    * chore: fix code formatting
    
    * chore: fix type formatting
    
    * content: change section order
    
    * style: add underline for h2

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -20,7 +20,9 @@
       </a>
     </div>
     <h2 id="projects">Projects</h2>
-    <RepositoryList username="jelleschutter"/>
+    <RepositoryList username="jelleschutter" />
+    <h2 id="jobs">Jobs</h2>
+    <WorkExperienceList :work_experiences="work_experiences" />
   </main>
   <footer>
     Jelle Schutter &copy; 2021
@@ -30,13 +32,47 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import RepositoryList from '@/components/RepositoryList.vue';
+import WorkExperienceList from '@/components/WorkExperienceList.vue';
+import type { WorkExperience as WorkExperienceModel } from '@/models/WorkExperience';
 
 export default defineComponent({
   components: {
     RepositoryList,
+    WorkExperienceList,
   },
-  data() {
+  data(): { work_experiences: WorkExperienceModel[]; email: string } {
     return {
+      work_experiences: [
+        {
+          company: 'watson News',
+          job_title: 'Data Analyst',
+          start_date: '2023-08-01',
+        },
+        {
+          company: 'BlueMouse GmbH',
+          job_title: 'Web Developer',
+          start_date: '2022-10-01',
+          end_date: '2023-07-31',
+        },
+        {
+          company: 'ABB',
+          job_title: 'Data Science Intern',
+          start_date: '2022-03-01',
+          end_date: '2023-02-28',
+        },
+        {
+          company: 'BlueMouse GmbH',
+          job_title: 'Web Developer',
+          start_date: '2020-07-01',
+          end_date: '2020-08-31',
+        },
+        {
+          company: 'BlueMouse GmbH',
+          job_title: 'Junior Web Developer',
+          start_date: '2019-07-01',
+          end_date: '2020-06-30',
+        },
+      ],
       email: atob('amVsbGVAc2NodXR0ZXIueHl6'),
     };
   },
@@ -60,8 +96,19 @@ main {
     width: 50%;
   }
 
-  h1, h2 {
+  h1,
+  h2 {
     text-align: center;
+  }
+
+  h2 {
+    display: inline-block;
+    position: relative;
+    left: 50%;
+    margin-top: 1.5em;
+    padding-bottom: 0.2em;
+    transform: translateX(-50%);
+    border-bottom: 3px solid $primary-color;
   }
 
   hr {
