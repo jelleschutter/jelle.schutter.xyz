31ab5aa701943670b7db84d8e39788b1234f478c
Jelle Schutter <jelle@schutter.xyz>
2024-07-07 13:37:02 +0200

    chore: reduce dependencies (#297)
    * chore: reduce dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faArchive, faCodeBranch } from '@fortawesome/free-solid-svg-icons';
-import { faCopyright, faEnvelope, faFile, faStar } from '@fortawesome/free-regular-svg-icons';
+import { faEnvelope, faFile, faStar } from '@fortawesome/free-regular-svg-icons';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App.vue';
@@ -9,12 +9,11 @@ import router from './router';
 
 library.add(faArchive);
 library.add(faCodeBranch);
-library.add(faCopyright);
 library.add(faEnvelope);
 library.add(faFile);
-library.add(faStar);
 library.add(faGithub);
 library.add(faLinkedin);
+library.add(faStar);
 
 const app = createApp(App).use(router);
 app.mount('#app');
